47feb55ed16d28f29d5b6e0600f493928c00b9e4
Nitipoom007 <s6603052421160@email.kmutnb.ac.th>
2025-01-09 11:36:13 +0700

    add class-components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './style.css'
 import { Header,Content,Footer } from "./func-components";
+import Calender from "./class-components";
 
 function App() {
   
   return (
     <>
       <Header/>
+      <p><center><Calender/></center></p>
       <Content/>
       <Footer/>
     </>
